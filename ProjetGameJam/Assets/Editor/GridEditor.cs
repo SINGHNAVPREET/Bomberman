880b6ae5bab7286190e48b623f1fa9d708035987
Stocky93420 <navpreet.mail@gmail.com>
2017-12-01 18:57:12 -0500

    Ajout Modif

---
+++ ProjetGameJam/Assets/Editor/GridEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Grid))]
+
+public class GridEditor : Editor 
+{
+	Grid grid;
+	private bool keyPressed = false;
+
+	public void OnEnable()
+	{
+		grid = (Grid)target;
+
+		SceneView.onSceneGUIDelegate += gridUpdate;
+	}
+
+	public void OnDisable()
+	{
+		SceneView.onSceneGUIDelegate -= gridUpdate;
+	}
+
+	public void gridUpdate(SceneView sceneView)
+	{
+		Event e = Event.current;
+
+		Ray rayon = Camera.current.ScreenPointToRay
+			(
+				new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight)
+			);
+
+		Vector3 mousePos = rayon.origin;
+
+		if (e.ToString () == "Repaint" && e.control && !keyPressed) {
+			keyPressed = true;
+
+			GameObject obj;
+
+			Object prefab = PrefabUtility.GetPrefabParent (Selection.activeObject);
+
+			obj = (GameObject)PrefabUtility.InstantiatePrefab (prefab);
+
+			Vector3 aligned = new Vector3 (
+				Mathf.Floor (mousePos.x / grid.width) * grid.width + grid.width * .5f,
+				Mathf.Floor (mousePos.y / grid.height) * grid.height + grid.height * .5f
+
+			);
+
+			obj.transform.position = aligned;
+		} 
+		else if (e.ToString () == "Repaint" && e.control && keyPressed)
+			keyPressed = false;
+	}
+
+}
---
+++ ProjetGameJam/Assets/Scripts/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[AddComponentMenu("Custom/Editor")]
+
+
+public class Grid : MonoBehaviour {
+
+	public float width = 1.0f;
+	public float height = 1.0f;
+
+	public Color color = Color.red;
+
+	void OnDrawGizmos()
+	{
+		Vector3 pos = Camera.current.transform.position;
+
+		Gizmos.color = color;
+
+		for (float y = pos.y - 250.0f; y < pos.y + 250.0f; y += height)
+		{
+			Gizmos.DrawLine
+			(
+				new Vector3(-250.0f, Mathf.Floor(y / height) * height, 0.0f),
+				new Vector3(250.0f, Mathf.Floor(y / height) * height, 0.0f)
+
+			);
+		}
+
+		for (float x = pos.x - 250.0f; x < pos.x + 250.0f; x += height)
+		{
+			Gizmos.DrawLine
+			(
+				new Vector3(Mathf.Floor(x / width) * width, -250.0f, 0.0f),
+				new Vector3(Mathf.Floor(x / width) * width, 250.0f, 0.0f)
+
+			);
+		}
+	}
+
+}
